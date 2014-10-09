1e4b383ecc2129c3cfc5564d847047ac4271476a
Henri <Henri@h2kker>
2014-10-09 10:57:46 +0300

    IntroUI

--- POS/src/ee/ut/math/tvt/PATS/Intro.java
+++ POS/src/ee/ut/math/tvt/PATS/Intro.java
@@ -1,5 +1,8 @@
 package ee.ut.math.tvt.PATS;
 
-public class Intro {
+public class Intro extends IntroUI{
+	public static void main(String[] args) {        
+		createWindow();
+	}
 
 }
--- POS/src/ee/ut/math/tvt/PATS/IntroUI.java
+++ POS/src/ee/ut/math/tvt/PATS/IntroUI.java
@@ -1,5 +1,22 @@
 package ee.ut.math.tvt.PATS;
 
-public class IntroUI {
+import java.awt.*;  
+import javax.swing.*;   
 
+// Create a simple GUI window 
+public class IntroUI {     
+	protected static void createWindow() {        
+		//Create and set up the window.        
+		JFrame frame = new JFrame("Simple GUI");       
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);         
+		JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER);        
+		
+		textLabel.setPreferredSize(new Dimension(300, 100));        
+		frame.getContentPane().add(textLabel, BorderLayout.CENTER);         
+		//Display the window.        
+		frame.setLocationRelativeTo(null);        
+		frame.pack();       
+		frame.setVisible(true); 
+	}     
+	 
 }
