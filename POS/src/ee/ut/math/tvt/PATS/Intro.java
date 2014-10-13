4be513bcf01f72e259c2993dfb0f4c56578e4307
Kulur <Kulur@XBOX>
2014-10-13 20:38:04 +0300

    teine katse

--- POS/src/ee/ut/math/tvt/PATS/Intro.java
+++ POS/src/ee/ut/math/tvt/PATS/Intro.java
@@ -1,17 +1,19 @@
 package ee.ut.math.tvt.PATS;
 
+import org.apache.log4j.Logger;
+
 
 public class Intro {
 	
 	static IntroUI intro;
 	public static void main(String[] args) {  
-		//final Logger LOGGER = Logger.getLogger(Intro.class);
-               
+		final Logger LOGGER = Logger.getLogger(Intro.class);
+		LOGGER.info("application start");    
 		
 		
-	 intro = new IntroUI ("Intro Window");
-	 intro.start();
-	// LOGGER.info("application start");
+		intro = new IntroUI ("Intro Window");
+		intro.start();
+	
 	}
 	
 }
